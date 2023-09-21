82a15fedf8d1660b967aa6884c3a0c2787b0c8e7
Jp Marin Diaz <jpmarindiaz@gmail.com>
2023-09-21 10:43:54 +0200

    setup

--- setup.R
+++ setup.R
@@ -1,7 +1,11 @@
 
-library(tidyverse)
-library(sf)
-library(leaflet)
+
+## Start Setup by restoring R packages 
+
+packrat::status()
+packrat::restore()
+
+## 
 
 if (!require("remotes", character.only = TRUE)) {
   install.packages("remotes", dependencies = TRUE)
@@ -9,13 +13,5 @@ if (!require("remotes", character.only = TRUE)) {
 if (!require("dsconnect", character.only = TRUE)) {
   remotes::install_github("datasketch/dsconnect")
 }
-
-
-## Start Setup by restoring R packages 
-
-packrat::status()
-packrat::restore()
-
-
 
 
